6d1b1f3df929278f062e23fe3fe44c9414edad71
Anteneh Yalew <yalewantenehtadesse@gmail.com>
2020-04-23 18:36:10 -0400

    Add procfile and update gitignore

--- app.js
+++ app.js
@@ -118,7 +118,11 @@ app.get("/showallcampbooks/:dozens", (req,res)=>{
 })
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-app.listen(process.env.PORT || "3000",()=>{
+app.listen(port,()=>{
     console.log("Connected to Server")
 })
